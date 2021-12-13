9f72c934775387e342f45d63fef1ce17a83c6afa
Cung Lian Sang <salaikyonekyone12@gmail.com>
2021-12-13 17:49:29 +0100

    add constant acceleration motion model in Helper script for KF

--- src/Helpers_KF.py
+++ src/Helpers_KF.py
@@ -16,7 +16,7 @@ def initConstVelocityKF():
     
     # Initial assumed values of the state which includes pose and their corresponding velocites
     x_0 = np.array([1.2, 1.82 , 0.885 , 1, 2, 1.5])  #  Initial guest
-    x_0.shape = (6,1)      # force to be a column vector 
+    x_0.shape = (len(x_0),1)      # force to be a column vector (6,1)
     
     # Initial value of State Covariance Matrix or Error Covariance 
     P_0 = np.multiply(2, np.eye(6))
@@ -41,7 +41,8 @@ def initConstVelocityKF():
                   [0, v_n[1], 0],
                   [0, 0, v_n[2]]])
     
-    '''Process Noise Covariance
+    '''
+    Process Noise Covariance
     Ref1: Survey of Maneuvering Target Tracking. Part I: Dynamic Models bz X. Rong Li and et.
     Ref2: Estimation with applications to Tracking and Navigation by Z. Bar-Shalom and et al. [chap. 6]
     Ref3: Mobile Positioning and Tracking (2nd Edition) by S. Frattasi & F. D. Rosa [sec: 6.4]
@@ -58,15 +59,6 @@ def initConstVelocityKF():
                     [0, 0, q_oD, 0, 0, q_lD]])
 
     Q = Q_0 * sigma_sq
-#       Q = np.multiply(Q_0, sigma_sq)
-                 
-#     Q = np.array([[np.multiply(v_n[0], q_uD), 0, 0, np.multiply(v_n[0], q_oD), 0, 0],
-#                           [0, np.multiply(v_n[1], q_uD), 0, 0, np.multiply(v_n[1], q_oD), 0 ],
-#                           [ 0,  0, np.multiply(v_n[2], q_uD ),  0 , 0 ,np.multiply(v_n[2], q_oD)],
-#                           [np.multiply(v_n[0], q_oD), 0, 0, np.multiply(v_n[0], q_lD), 0, 0],
-#                           [0, np.multiply(v_n[1], q_oD), 0, 0, np.multiply(v_n[1], q_lD), 0],
-#                           [0, 0, np.multiply(v_n[1], q_oD), 0, 0, np.multiply(v_n[1], q_lD)] ])
-    
     
     return A, B, H, Q, R, P_0, x_0 
 
@@ -81,9 +73,60 @@ def initConstAccelerationKF():
     v_n = np.array([ 0.0225, 0.0225,  0.08])
     sigma_sq = 0.01    # Process noise value based on 10cm precision
 
-    #######  TODO #########
+    # Initial guest for state vector 
+    x_0 = np.array([1.2, 1.82 , 0.885 , 1, 2, 1.5, 0.5, 1.0, 0.75])  #  Initial guest
+    x_0.shape = (len(x_0),1)      # force to be a column vector  (9, 1)
+
+    # Initial guese of State Covariance Matrix shoudn't be zeros 
+    P_0 = np.multiply(2, np.eye(len(x_0)))  # (9x9)
+
+
+    # State Model or Transition Matrix  (9x9)
+    A_oD = (np.power(dt, 2))/2  # coefficient for acceleration (off-diagonal)
+    A = np.array([[1, 0, 0, dt, 0, 0, A_oD, 0, 0],
+                  [0, 1, 0, 0, dt, 0, 0, A_oD, 0],
+                  [0, 0, 1, 0, 0, dt, 0, 0, A_oD],
+                  [0, 0, 0, 1, 0, 0, dt, 0, 0],
+                  [0, 0, 0, 0, 1, 0, 0, dt, 0],
+                  [0, 0, 0, 0, 0, 1, 0, 0, dt],
+                  [0, 0, 0, 0, 0, 0, 1, 0, 0],
+                  [0, 0, 0, 0, 0, 0, 0, 1, 0],
+                  [0, 0, 0, 0, 0, 0, 0, 0, 1]])
+
+    B = 0 # No control input
     
+    #  Relation b/w the observed Measurement and the State vector (3x9)
+    H = np.array([[1, 0, 0, 0, 0, 0, 0, 0, 0],
+                  [0, 1, 0, 0, 0, 0, 0, 0, 0],
+                  [0, 0, 1, 0, 0, 0, 0, 0, 0]])
     
-    # return A, B, H, Q, R, P_0, x_0 
+    # Measurement Noise Covariance (3x3) 
+    R = np.array([[v_n[0], 0, 0],
+                  [0, v_n[1], 0],
+                  [0, 0, v_n[2]]])
+    
+    '''
+    Process Noise Covariance (9x9)
+    Ref1: Survey of Maneuvering Target Tracking. Part I: Dynamic Models bz X. Rong Li and et.
+    Ref2: Estimation with applications to Tracking and Navigation by Z. Bar-Shalom and et al. [chap. 6]
+    Ref3: Mobile Positioning and Tracking (2nd Edition) by S. Frattasi & F. D. Rosa [sec: 6.4]
+    '''
+    q_t4 = (np.power(dt, 4))/4   # (dt, 3)/3  in some lit.
+    q_t3 = (np.power(dt, 3))/2   # (dt, 2)/2  in some lit.
+    q_t2 = np.square(dt)         # dt         in some lit.
+    
+    Q_0 = np.array([[q_t4, 0, 0, q_t3, 0, 0, q_t2, 0, 0],
+                    [0, q_t4, 0, 0, q_t3, 0, 0, q_t2, 0],
+                    [0, 0, q_t4, 0, 0, q_t3, 0, 0, q_t2],
+                    [q_t3, 0, 0, q_t2, 0, 0, dt, 0, 0],
+                    [0, q_t3, 0, 0, q_t2, 0, 0, dt, 0],
+                    [0, 0, q_t3, 0, 0, q_t2, 0, 0, dt]
+                    [q_t2, 0, 0, dt, 0, 0, 1, 0, 0],
+                    [0, q_t2, 0, 0, dt, 0, 0, 1, 0],
+                    [0, 0, q_t2, 0, 0, dt, 0, 0, 1]])
+
+    Q = Q_0 * sigma_sq
+    
+    return A, B, H, Q, R, P_0, x_0 
         
         
